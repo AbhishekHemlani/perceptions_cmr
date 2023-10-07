c7501d3d187d1958822f0240307ab8a33014c9f8
Andrew Yuan <andrewjyuan@gmail.com>
2023-10-07 15:06:04 -0400

    worked on dataloader functions

--- data/utils/dataloader.py
+++ data/utils/dataloader.py
@@ -1,13 +1,35 @@
 import numpy as np
 import os
+import cv2
+import time
 
 class DataLoader():
-    def __init__(self, instance):
-        self.instance = np.load(instance)
+    def __init__(self, data):
+        self.instances = os.listdir(data)
     
-    def getlen(self):
-        return np.size(self.instance)
+    def __len__(self):
+        return len(self.instances)
     
-    def getIn(self):
-        pass
+    def __getitem__(self, index):
+        return np.load('./data/np-data/instance-' + str(index) + '.npz')
+    
+def main():
+    data_loader = DataLoader('./data/np-data/')
+    print(len(data_loader))
+    print(data_loader[5])
+    
+    while True:
+        for i in range(len(data_loader)):
+            left_img = data_loader[i]["left"] # H x W x 3 (np.uint8) image
+            cv2.imshow("Test", left_img)
+            cv2.waitKey(1)
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+# d = {"left": left, "right": right, "point": point}
+# np.savez("instance-{i}.npz", d)
     
